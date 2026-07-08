879edd93364440397f33a2c7c8007a5fa868df07
dynamicbeam <359429108@qq.com>
2026-07-08 16:10:50 +0800

    Revert "悬停8秒后隐藏"
    This reverts commit 9c3e44edb4b2166a6a3b99422c91c6d232702027.

--- app/src/main/java/com/yukon/litewebtv/MainViewModel.kt
+++ app/src/main/java/com/yukon/litewebtv/MainViewModel.kt
@@ -51,7 +51,6 @@ class MainViewModel : ViewModel() {
     private var pendingOsdShow = false
 
     private var loadingTimeoutJob: Job? = null
-    private var sidebarJob: Job? = null
 
     private fun startLoadingTimeout() {
         loadingTimeoutJob?.cancel()
@@ -119,23 +118,9 @@ class MainViewModel : ViewModel() {
         }
     }
 
-    private fun resetSidebarTimer() {
-        sidebarJob?.cancel()
-        sidebarJob = viewModelScope.launch {
-            delay(8000)
-            _showChannelSidebar.value = false
-            _showProgramSidebar.value = false
-        }
-    }
-
     fun toggleChannelSidebar(show: Boolean) {
         _showChannelSidebar.value = show
-        if (show) {
-            _showProgramSidebar.value = false
-            resetSidebarTimer()
-        } else {
-            sidebarJob?.cancel()
-        }
+        if (show) _showProgramSidebar.value = false
     }
 
     fun toggleProgramSidebar(show: Boolean) {
@@ -143,9 +128,6 @@ class MainViewModel : ViewModel() {
         if (show) {
             _showChannelSidebar.value = false
             _jsCommand.tryEmit("window.LiteWebTV.extractPrograms();")
-            resetSidebarTimer()
-        } else {
-            sidebarJob?.cancel()
         }
     }
 
